cfc03afcb0f40d1efc566f4cbf5e6a7a01767204
Mehdi Bounya <mehdi.bounya@gmail.com>
2019-12-02 01:09:14 +0100

    get_students: include students from 42cursus

--- get_students.php
+++ get_students.php
@@ -9,29 +9,40 @@ if ($argc != 2)
 }
 
 $campus_id = (int) $argv[1];
-$data = [
-    'filter[campus_id]' => $argv[1],
-    'filter[blackholed]' => 'false',
-    'sort' => 'created_at',
-    'page' => 1,
+
+$cursus_ids = [
+	1,	// 42
+	21,	// 42cursus
 ];
 
-do {
-    $response = $intraRequest->get("/v2/cursus/1/cursus_users", $data);
-    $response = json_decode($response);
-    foreach ($response as $cursus_user)
-    {
-        $user_details = $intraRequest->get('/v2/users/'.$cursus_user->user->id);
-        $user_details = json_decode($user_details);
-        if ($user_details->{'staff?'})
-            continue;
-        $row = [
-            $user_details->login,
-            $user_details->first_name,
-            $user_details->last_name,
-            $user_details->email,
-        ];
-        fputcsv(STDOUT, $row);
-    }
-    $data['page']++;
-} while (count($response) > 0);
+fputcsv(STDOUT, ['login', 'first_name', 'last_name', 'email', 'level']);
+
+foreach ($cursus_ids as $cursus_id) {
+	$data = [
+		'filter[campus_id]' => $argv[1],
+		'filter[blackholed]' => 'false',
+		'sort' => 'created_at',
+		'page' => 1,
+	];
+	do {
+		$response = $intraRequest->get("/v2/cursus/$cursus_id/cursus_users", $data);
+		$response = json_decode($response);
+		foreach ($response as $cursus_user)
+		{
+			$user_details = $intraRequest->get('/v2/users/'.$cursus_user->user->id);
+			$user_details = json_decode($user_details);
+			if ($user_details->{'staff?'})
+				continue;
+			$row = [
+				$user_details->login,
+				$user_details->first_name,
+				$user_details->last_name,
+				$user_details->email,
+			];
+			fputcsv(STDOUT, $row);
+		}
+		$data['page']++;
+	} while (count($response) > 0);
+}
+
+
